11e6ef2ccbe48cf4d4d6f48b5b0731e75bd4df32
Laguna1 <lagunazoomarket@gmail.com>
2020-10-18 04:57:09 +0300

    add users features

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 2)
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome to the Oksana`s blog #{@user.username}"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user)
+      flash[:success] = 'Your account was updated successfully'
+      redirect_to articles_path
+    end
+  end
+
+  def show
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all User's articles have been deleted"
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
